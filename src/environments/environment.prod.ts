8879150d059ca563dbd71ab815b2b173cf0bc3ce
William Sant Ana <williamprogphp@yahoo.com.br>
2024-12-09 23:39:26 -0300

    Add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://ec2-3-129-57-190.us-east-2.compute.amazonaws.com/api',
+    apiBaseUrl: 'http://ec2-3-16-13-189.us-east-2.compute.amazonaws.com/api/',
 };
   
